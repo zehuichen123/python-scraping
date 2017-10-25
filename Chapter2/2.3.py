b798dc237311b85d533d33c6645b95b75f1ee6f7
loveSnowBest <lovesnowbest@gmail.com>
2017-10-25 20:48:31 +0800

    till page 36

---
+++ Chapter2/2.3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+from urllib.request import urlopen
+# from urllib.error import HTTPError,URLError
+from bs4 import BeautifulSoup
+import datetime
+import random
+import re
+
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+	html=urlopen('http://en.wikipedia.org'+articleUrl)
+	BSObj=BeautifulSoup(html,'html.parser')
+	return BSObj.find('div',{'id':"bodyContent"}).findAll('a',
+		{'href':re.compile('^(/wiki/)((?!:).)*$')})
+links=getLinks("/wiki/Kevin_Bacon")
+while len(links)>0:
+	newArticle=links[random.randint(0,len(links)-1)].attrs["href"]
+	print(newArticle)
+	links=getLinks(newArticle)
